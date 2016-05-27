6bcdd5372c44ecf96b2b9e5051f97223d5380ce4
Tapuy <jepiwcp@gmail.com>
2016-05-27 10:03:23 +0700

    Add files via upload

---
+++ back.js
@@ -0,0 +1,5 @@
+ListBlogLink = "http://www.animepjm.com";
+ListCount = 8;
+TitleCount = 70;
+ListLabel = "data:label.name";
+document.write('<script src="'+ListBlogLink+'/feeds/posts/default/-/'+ListLabel+'?alt=json-in-script&callback=mbtlist"></'+'script>');
