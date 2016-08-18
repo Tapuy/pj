d431719db10a083f8dd5333cabf99ffe11a026ac
Tapuy <jepiwcp@gmail.com>
2016-08-18 13:39:00 +0700

    Update nonblock.js

--- nonblock.js
+++ nonblock.js
@@ -1,23 +1,28 @@
 function addLink() {
+    console.log('hey');
     var body_element = document.getElementsByTagName('body')[0];
+
     var selection;
     selection = window.getSelection();
-     var htmlDiv = document.createElement("div");
+
+    // added lines begin
+    var htmlDiv = document.createElement("div");
     for (var i = 0; i < selection.rangeCount; ++i) {
         htmlDiv.appendChild(selection.getRangeAt(i).cloneContents()); 
     } 
-    var selectionHTML = htmlDiv.innerHTML;     
+    var selectionHTML = htmlDiv.innerHTML;    
+    // added lines end
+
     var pagelink = "<br /><br /> Read more at animepjm.com - <a href='"+document.location.href+"'>"+document.location.href+"</a><br />";
-    body_element.setAttribute("blockquote", false);
-    var copytext = selectionHTML + pagelink;
+    var copytext = selectionHTML + pagelink;    
     var newdiv = document.createElement('div');
     newdiv.style.position='absolute';
     newdiv.style.left='-99999px';
     body_element.appendChild(newdiv);
     newdiv.innerHTML = copytext;
     selection.selectAllChildren(newdiv);
-    window.setTimeout(function() {
+    window.setTimeout(function () {
         body_element.removeChild(newdiv);
-    },0);
+    }, 0);
 }
-  document.oncopy = addLink;
+document.oncopy = addLink;
