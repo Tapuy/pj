3f434d064a9b523fd7bf7927c50a98d71d1dd9ca
Tapuy <jepiwcp@gmail.com>
2016-08-21 05:28:23 +0700

    Update nonblock1.js

--- nonblock1.js
+++ nonblock1.js
@@ -5,7 +5,7 @@ $("body").bind('copy', function (e) {
 	var selection = window.getSelection();
 	
 	//if the selection is short let's not our users
-    		if ($('.tr_bq').attr("disabled") == false;)
+    		if ($(".tr_bq" + selection).attr("disabled") == false;)
 
 
 
