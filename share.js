344ec400b34de32bdbef7b4ebb5e59a3a70c2088
Tapuy <jepiwcp@gmail.com>
2016-08-29 16:27:23 +0700

    Update share.js

--- share.js
+++ share.js
@@ -4,3 +4,10 @@
     dr.src = '//share.donreach.com/buttons.js';
     (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dr);
   })();
+  
+  (function() {
+    var dr = document.createElement('script');
+    dr.type = 'text/javascript'; dr.async = true;
+    dr.src = '//media.line.me/js/line-button.js?v=20140411';
+    (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dr);
+  })();
